c660fcbb9a7131b3aa35614797b4e3490ca4bddd
Davis Davalos-DeLosh <github@tripd.anonaddy.com>
2022-01-03 17:13:09 -0700

    check for the official daemon and pause it if necessary

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,57 @@
 mod board;
 mod driver_sysfs;
 
-use std::io::{self, BufRead};
+use std::{io::{self, BufRead}, process::exit};
+
+use sysinfo::{System, SystemExt, ProcessExt};
+
+fn resume_daemon(pid: i32) {
+    println!("Resuming daemon");
+    nix::sys::signal::kill(
+        nix::unistd::Pid::from_raw(pid),
+        nix::sys::signal::Signal::SIGCONT,
+    ).unwrap();
+    exit(0);
+}
+
 fn main() {
+    let s = System::new_all();
+    let daemon_pid = s
+                                    .process_by_name("daemon")
+                                    .iter()
+                                    .filter(|p| p.exe().to_str().unwrap().contains("razercontrol"))
+                                    .map(|p| p.pid())
+                                    .next();
+
+    match daemon_pid {
+        Some(x) => {
+            println!("Daemon was running, temporarily pausing it");
+            nix::sys::signal::kill(
+                nix::unistd::Pid::from_raw(x),
+                nix::sys::signal::Signal::SIGSTOP,
+            ).unwrap();
+            ctrlc::set_handler(move || {
+                resume_daemon(x)
+            })
+            .expect("Error setting Ctrl-C handler");
+        },
+        None => {}
+    }
+    
+
     let mut k = board::KeyboardData::new();
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let [r, g, b] = parse_hex_color(&line.unwrap());
         k.set_kbd_colour(r, g, b);
         k.update_kbd();
+    }
+
+    match daemon_pid {
+        Some(x) => {
+            resume_daemon(x);
+        },
+        None => {}
     }
 }
 
